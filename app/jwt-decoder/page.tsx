adf41939615690accc49dc03b543febcb9e04ea4
hientuminh <hientuminh@gmail.com>
2024-10-12 23:13:55 +0700

    initial

---
+++ app/jwt-decoder/page.tsx
@@ -0,0 +1,8 @@
+export default function JwtDecoder() {
+    return (
+      <div className="flex flex-col">
+        This is the JwtDecoder page.
+      </div>
+    );
+  }
+  
---
+++ app/jwt-encoder/page.tsx
@@ -0,0 +1,8 @@
+export default function JwtEncoder() {
+    return (
+      <div className="flex flex-col">
+        This is the JwtEncoder page.
+      </div>
+    );
+  }
+  
